40af16b42f75625b4b86c502eb8eb189401f98cd
Gregory <lubeevgs2000@gmail.com>
2025-05-09 18:40:31 +0300

    Some of my programs

---
+++ src/ASCII.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ASCII {
+    public static void main(String[] args) {
+        System.out.print("Введите символ: ");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+
+        while (str.length() != 1) {
+            System.out.println("Введите символ а не строку: ");
+
+        }
+        int ascii = (int) str.charAt(0);
+
+        System.out.println(ascii);
+
+    }
+
+}
