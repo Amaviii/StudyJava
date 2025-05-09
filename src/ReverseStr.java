12fdc36619c6b62baf115902cb460ba5c828bbe0
Gregory <lubeevgs2000@gmail.com>
2025-05-09 19:29:49 +0300

    Some of my programs

---
+++ src/ReverseStr.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReverseStr {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        String result = rev(str);
+        System.out.println(result);
+    }
+
+    public static String rev(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char c = str.charAt(i);
+            sb.append(c);
+        }
+
+
+        return sb.toString();
+    }
+
+}
